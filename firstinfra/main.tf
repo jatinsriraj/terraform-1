4874b262616b72354428687bea0702c3512f3daf
jatinsriraj <jatinsri2@gmail.com>
2023-09-12 10:49:21 +0530

    new commit

--- firstinfra/main.tf
+++ firstinfra/main.tf
@@ -1,7 +1,8 @@
-provider "aws" {
-    region = "ap-south-1"  
-}
+resource "aws_vpc" "jatin" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
-resource "aws_vpc" "first_vpc" {
-    cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "automated"
+  }
 }
