d05602e2162785d90c2a28e2b42f4242d260e9ec
jatinsriraj <68094376+jatinsriraj@users.noreply.github.com>
2023-09-12 11:29:43 +0530

    Delete VPC/provider.tf

--- VPC/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  region     = "ap-south-1"  # Replace with your desired AWS region
-  access_key = "AKIA5UBTSTSBG3YA45PJ"
-  secret_key = "hLl5ycwnbP1lQlRMR+s87srVHGEmE3iVk+0YPG9x"
-}
